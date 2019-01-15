3865ec175699d7d0197a6bc3be9946fad03718f3
xx <qq@a.com>
2019-01-15 20:17:29 +0800

    添加敏感字符

--- main.c
+++ main.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+     int coude;
+    int index;
+    char arr[1024];
+    printf("guanlimingancixitong\n");
+    while (1)
+    {
+        printf("1----tianjia\n");
 
-    printf("hello world!");
-
+        printf("2----shanchuzuihouyigeminganzifu\n");
+        printf("3----chakansuoyouminganzifu\n");
+        printf("4---tianjia\n");
+        printf("5-----tuichu\n");
+        printf("xuanzhexiangyinggongneng\n");
+        scanf("%d", &coude);
+    }
+    if(coude==1){
+        char x;
+     
+         printf("tianjia\n");
+         scanf("%c",&x);
+         scanf("%c",&x);
+         arr[index]=x;
+         index++;
+              char i;
+          printf("jixu\n");
+          scanf("%c",&i);
+          scanf("%c",&i);
+    }
     return 0;
-
 }
