73f277e047c28f7d2d245d4191436109507243e8
xx <qq@a.com>
2019-01-15 21:30:16 +0800

    退出

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-     int coude;
+    int coude;
     int index;
     char arr[1024];
     printf("guanlimingancixitong\n");
@@ -17,63 +17,83 @@ int main()
         printf("xuanzhexiangyinggongneng\n");
         scanf("%d", &coude);
     }
-    if(coude==1){
+    if (coude == 1)
+    {
         char x;
         char i;
-         printf("tianjia\n");
-         scanf("%c",&x);
-         scanf("%c",&x);
-         arr[index]=x;
-         index++;
-          printf("jixu\n");
-          scanf("%c",&i);
-          scanf("%c",&i);
+        printf("tianjia\n");
+        scanf("%c", &x);
+        scanf("%c", &x);
+        arr[index] = x;
+        index++;
+        printf("jixu\n");
+        scanf("%c", &i);
+        scanf("%c", &i);
     }
-    if(coude==2){
+    if (coude == 2)
+    {
         char x;
         printf("shanchu\n");
-        if(index>0){
+        if (index > 0)
+        {
             index--;
             printf("chengongjixu\n");
-        scanf("%c",&x);
-        scanf("%c",&x);
-        }else{
+            scanf("%c", &x);
+            scanf("%c", &x);
+        }
+        else
+        {
             printf("shibaijixu\n");
-            scanf("%c",&x);
-            scanf("%c",&x);
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
-        if(coude==3){
-             char x;
-             if(index>0){
-                 for(int v=0;v<index;v++){
-                     printf("di %d gezifu%c\n",v+1,arr[v]);
-                 }
-             }else{
-                 printf("wu");
-             }
-             printf("jixu\n");
-             scanf("%c",&x);
-             scanf("%c",&x);
+        if (coude == 3)
+        {
+            char x;
+            if (index > 0)
+            {
+                for (int v = 0; v < index; v++)
+                {
+                    printf("di %d gezifu%c\n", v + 1, arr[v]);
+                }
+            }
+            else
+            {
+                printf("wu");
+            }
+            printf("jixu\n");
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
-        if(coude==4){
+        if (coude == 4)
+        {
             printf("qingshuruyiduanyingwen\n");
             char content[1024];
-            scanf("%s",content);
-            for(int i = 0;1;i++){
-                if(content[i]== '\0'){
+            scanf("%s", content);
+            for (int i = 0; 1; i++)
+            {
+                if (content[i] == '\0')
+                {
                     break;
                 }
-            
-            if(ismingan(arr[i] ==1 )){
-                content[i]='*';
+
+                if (ismingan(arr[i] == 1))
+                {
+                    content[i] = '*';
+                }
+                printf("%c", content[i]);
             }
-            printf("%c",content[i]);
+            printf("\n");
+            printf("tihuanwancdianjihuichejix\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
-        printf("\n");
-        printf("tihuanwancdianjihuichejix\n");
-        char x;
-        scanf("%c",&x);
-        scanf("%c",&x);
+        if (coude == 5)
+        {
+            printf("tuichu\n");
+            break;
+        
         }
     }
     return 0;
